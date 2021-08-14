7ff4097fb6d053f2757711068c8597931458732e
Alex Gokhale <hello@alexgokhale.com>
2021-08-14 16:03:20 +0100

    Add documentation to categories

--- abuseipdb.go
+++ abuseipdb.go
@@ -28,28 +28,58 @@ type Category int
 
 // A list of the categories supported by the AbuseIPDB API.
 const (
+	// CategoryDNSCompromise includes abuse which involves altering DNS records resulting in improper redirection.
 	CategoryDNSCompromise Category = iota + 1
+	// CategoryDNSPoisoning includes abuse which involves falsifying domain server cache (cache poisoning).
 	CategoryDNSPoisoning
+	// CategoryFraudOrders includes abuse which involves making fraudulent purchases/orders online.
 	CategoryFraudOrders
+	// CategoryDDoSAttack includes abuse involving participating in distributed denial-of-service (usually as part of a botnet).
 	CategoryDDoSAttack
+	// CategoryFTPBruteForce includes abuse involving brute-force credential attacks against FTP servers.
 	CategoryFTPBruteForce
+	// CategoryPingOfDeath includes abuse involving sending oversized IP packets.
 	CategoryPingOfDeath
+	// CategoryPhishing includes abuse involving phishing websites or emails.
 	CategoryPhishing
+	// CategoryFraudVOIP includes abuse involving spam/scam calls from VoIP numbers.
 	CategoryFraudVOIP
+	// CategoryOpenProxy describes IPs acting as open proxies, relays or Tor exit nodes.
 	CategoryOpenProxy
+	// CategoryWebSpam includes abuse involving comment/forum spam, HTTP referer spam or other CMS-related spam.
 	CategoryWebSpam
+	// CategoryEmailSpam includes abuse involving spam email content, infected attachments and phishing.
 	CategoryEmailSpam
+	// CategoryBlogSpam includes abuse involving comment spam on CMS blogs.
 	CategoryBlogSpam
+	// CategoryVPNIP is a conjunctive category for VPN servers.
 	CategoryVPNIP
+	// CategoryPortScan includes abuse involving scanning the internet for open ports/vulnerable devices.
 	CategoryPortScan
+	// CategoryHacking includes abuse involving all types of unauthorised system access.
+	// This should be used in combination with other categories.
 	CategoryHacking
+	// CategorySQLInjection includes abuse involving any form of SQL injection attempt.
 	CategorySQLInjection
+	// CategorySpoofing includes abuse involving spoofing email sender information.
 	CategorySpoofing
+	// CategoryBruteForce includes abuse involving brute-force credential attacks on a variety of protocols.
+	// Examples include SSH, FTP, STMP, RDP as well as webpage logins.
 	CategoryBruteForce
+	// CategoryBadWebBot includes abuse involving website scraping which doesn't honour robots.txt.
+	// Excessive requests and spoofed user agents can also be reported under this category.
 	CategoryBadWebBot
+	// CategoryExploitedHost includes abuse in which the host is likely infected with malware,
+	// and is being used for other attacks/hosting malicious content.
 	CategoryExploitedHost
+	// CategoryWebAppAttack includes abuse involving attempts to probe/exploit web applications.
+	// Examples include, CMS' such as WordPress, Drupal, phpMyAdmin, etc.
 	CategoryWebAppAttack
+	// CategorySSH includes abuse of Secure Shell (SSH).
+	//Use this category in combination with more specific categories.
 	CategorySSH
+	// CategoryIoTTargeted includes abuse targeting IoT devices.
+	// Include information about device type in report comments.
 	CategoryIoTTargeted
 )
 
